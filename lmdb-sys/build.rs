c50948987830f315e1e9164b18df5d04a46b53e8
zhangsoledad <787953403@qq.com>
2017-09-04 15:15:09 +0800

    specified opt_level

--- lmdb-sys/build.rs
+++ lmdb-sys/build.rs
@@ -1,25 +1,9 @@
-extern crate pkg_config;
 extern crate gcc;
 
-use std::env;
-use std::path::PathBuf;
-
 fn main() {
-
-    let mut lmdb: PathBuf = PathBuf::from(&env::var("CARGO_MANIFEST_DIR").unwrap());
-    lmdb.push("lmdb");
-    lmdb.push("libraries");
-    lmdb.push("liblmdb");
-
-    let mut mdb: PathBuf = lmdb.clone();
-    let mut midl: PathBuf = lmdb.clone();
-
-    mdb.push("mdb.c");
-    midl.push("midl.c");
-
-    if !pkg_config::find_library("liblmdb").is_ok() {
-        gcc::compile_library("liblmdb.a",
-                             &[(*mdb).to_str().unwrap(),
-                               (*midl).to_str().unwrap()]);
-    }
+    gcc::Build::new()
+            .file("lmdb/libraries/liblmdb/mdb.c")
+            .file("lmdb/libraries/liblmdb/midl.c")
+            .opt_level(2)
+            .compile("iblmdb.a");
 }
